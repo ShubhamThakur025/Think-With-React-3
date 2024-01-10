fb6ebac861c2e99f8199fa1a47b4491540582a41
ShubhamThakur025 <thakur0805@gmail.com>
2024-01-10 13:00:36 +0530

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id = "kalvium-text" className='logo'>
+      Hello Kalvium
+        </div>
+    </>
+  )
+}
+
+export default App
